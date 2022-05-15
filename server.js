cf402e96df51d3ae2251cce0441b53bc6f011e45
Fernandojussec <62914450+Fernandojussec@users.noreply.github.com>
2022-05-14 21:48:00 -0300

    criado o servidor com express

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = riquere('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+
+    res.status(200).send({ message: 'Olá mundo estou funcionando'})
+
+})
+
+
+app.listen(3001, () => {
+
+    console.log('Api rodando na porta 3001')
+
+})
